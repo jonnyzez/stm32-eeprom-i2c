0ca3e693047cccf6098f4e0c6586aad509742e2d
Jonny <jonnyzez@gmail.com>
2021-05-14 08:05:22 -0400

    adicionei a bibliotecas

---
+++ ZEZ-24CXX.h
@@ -0,0 +1,23 @@
+//========================================================================================
+//                             lib eeprom 24Cxx
+//
+//    14/05/21                                                                Jonny zez
+//========================================================================================
+
+
+#ifndef INC_EEPROM_H_
+#define INC_EEPROM_H_
+
+#include "stdint.h"
+#include "stm32f1xx_hal.h"
+
+void write_eeprom(uint16_t end, uint8_t data);
+
+void EEPROM_Write (uint16_t page, uint16_t offset, uint8_t *data, uint16_t size);
+void EEPROM_Read (uint16_t page, uint16_t offset, uint8_t *data, uint16_t size);
+void EEPROM_PageErase (uint16_t page);
+
+void EEPROM_Write_NUM (uint16_t page, uint16_t offset, float  fdata);
+float EEPROM_Read_NUM (uint16_t page, uint16_t offset);
+uint8_t read_eeprom(uint16_t end);
+#endif /* INC_EEPROM_H_ */
